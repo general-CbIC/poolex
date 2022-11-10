7fd2e75399ca4fbc34803e7a49f41d0a91db6daa
Aleksandr Sysoev <sanchous.ok@gmail.com>
2022-11-10 18:22:38 +0700

    [Test] state: valid after holding some workers

--- test/poolex_test.exs
+++ test/poolex_test.exs
@@ -5,7 +5,7 @@ defmodule PoolexTest do
   @pool_name :test_pool
 
   describe "state" do
-    test "is valid after initialization" do
+    test "valid after initialization" do
       Poolex.start_link(@pool_name,
         worker_module: Agent,
         worker_args: fn 0 -> 0 end,
@@ -19,6 +19,31 @@ defmodule PoolexTest do
       assert state.busy_workers_pids == []
       assert state.idle_workers_count == 5
       assert Enum.count(state.idle_workers_pids) == 5
+      assert state.worker_module == Agent
+      assert state.worker_args == [fn 0 -> 0 end]
+    end
+
+    test "valid after holding some workers" do
+      Poolex.start_link(@pool_name,
+        worker_module: Agent,
+        worker_args: fn 0 -> 0 end,
+        workers_count: 5
+      )
+
+      spawn(fn ->
+        Poolex.run(@pool_name, fn _pid ->
+          :timer.sleep(:timer.seconds(5))
+          :ok
+        end)
+      end)
+
+      state = Poolex.get_state(@pool_name)
+
+      assert state.__struct__ == Poolex.State
+      assert state.busy_workers_count == 1
+      assert Enum.count(state.busy_workers_pids) == 1
+      assert state.idle_workers_count == 4
+      assert Enum.count(state.idle_workers_pids) == 4
       assert state.worker_module == Agent
       assert state.worker_args == [fn 0 -> 0 end]
     end
