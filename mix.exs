b5c9dbcbcd8dcbf98f6b7cc3bf46198124d44048
Aleksandr Sysoev <sanchous.ok@gmail.com>
2022-12-29 18:16:53 +0700

    Set minimum elixir version to 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Poolex.MixProject do
     [
       app: :poolex,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
