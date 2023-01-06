7a0fd85e8600a8bd69ade1dedf7eb6af19308bcc
Aleksandr Sysoev <sanchous.ok@gmail.com>
2023-01-06 18:39:16 +0700

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Poolex.MixProject do
   def project do
     [
       app: :poolex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
