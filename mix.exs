f9efcd616011080cc39c42e07a74d8dec29c3d9b
Aleksandr Sysoev <sanchous.ok@gmail.com>
2023-03-25 16:08:44 +0700

    Support `diff` syntax highlighting

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule Poolex.MixProject do
       {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
       {:doctor, ">= 0.0.0", only: [:dev], runtime: false},
       {:ex_check, "~> 0.15.0", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:makeup_diff, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 
