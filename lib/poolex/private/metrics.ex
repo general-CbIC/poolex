b031748be829e56407dd7a76a32738ca18e66c52
Aleksandr Sysoev <sanchous.ok@gmail.com>
2023-11-28 18:53:39 +0700

    Metrics.start_poller function

--- lib/poolex/private/metrics.ex
+++ lib/poolex/private/metrics.ex
@@ -1,4 +1,4 @@
-defmodule Poolex.Metrics do
+defmodule Poolex.Private.Metrics do
   @moduledoc """
   Functions for dispatching metrics.
   """
@@ -13,6 +13,19 @@ defmodule Poolex.Metrics do
         idle_workers_count: debug_info.idle_workers_count
       },
       %{pool_id: pool_id}
+    )
+  end
+
+  @spec start_poller(Poolex.pool_id()) :: GenServer.on_start()
+  def start_poller(pool_id) do
+    name = :"#{pool_id}_metrics_poller"
+
+    :telemetry_poller.start_link(
+      measurements: [
+        {Poolex.Private.Metrics, :dispatch_pool_size_metrics, [pool_id]}
+      ],
+      period: :timer.seconds(1),
+      name: name
     )
   end
 end
