f95bd518305975f91595bbeffe4ebf1faf586e8d
Aleksandr Sysoev <general@cbic-dev.com>
2024-03-29 15:54:37 +0700

    Attach pool_size metrics in poolex_example app

--- examples/poolex_example/lib/poolex_example/application.ex
+++ examples/poolex_example/lib/poolex_example/application.ex
@@ -9,8 +9,16 @@ defmodule PoolexExample.Application do
        pool_id: :worker_pool,
        worker_module: PoolexExample.Worker,
        workers_count: 5,
-       max_overflow: 2}
+       max_overflow: 2,
+       pool_size_metrics: true}
     ]
+
+    :telemetry.attach(
+      "poolex_metrics",
+      [:poolex, :metrics, :pool_size],
+      &PoolexExample.MetricsHandler.handle_event/4,
+      nil
+    )
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
