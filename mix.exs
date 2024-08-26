9ff66d713f2263c0fbed29d45353b50f0791fad8
Aleksandr Sysoev <sanchous.ok@gmail.com>
2024-08-26 17:58:00 +0700

    Bump version to 0.10.0

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Poolex.MixProject do
       package: package(),
       source_url: "https://github.com/general-CbIC/poolex",
       start_permanent: Mix.env() == :prod,
-      version: "0.9.0"
+      version: "0.10.0"
     ]
   end
 
