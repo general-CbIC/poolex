c5d8b38b0d3d335983bb3b1d526fb6766b914d97
Hissssst <37012324+hissssst@users.noreply.github.com>
2024-12-08 08:29:50 +0000

    Several things I find useful (#117)
    * Monitoring without agent, high priority and enforce keys in state struct
    * Deprecates Poolex.get_state in favor of :sys.get_state
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Aleksandr Sysoev <general@cbic-dev.com>

--- lib/poolex/private/monitoring.ex
+++ lib/poolex/private/monitoring.ex
@@ -1,42 +1,26 @@
 defmodule Poolex.Private.Monitoring do
   @moduledoc false
+  alias Poolex.Private.State
+
   @type kind_of_process() :: :worker | :waiting_caller
 
-  @spec init() :: {:ok, pid()}
-  @doc """
-  Create new monitoring references storage.
-  """
-  def init do
-    Agent.start_link(fn -> %{} end)
-  end
-
-  @spec stop(pid()) :: :ok
-  @doc """
-  Delete storage.
-  """
-  def stop(pid) do
-    Agent.stop(pid)
-  end
-
-  @spec add(monitor_pid :: pid(), worker_pid :: pid(), kind_of_process()) :: :ok
+  @spec add(State.t(), worker_pid :: pid(), kind_of_process()) :: State.t()
   @doc """
   Start monitoring given worker or caller process.
   """
-  def add(monitor_pid, process_pid, kind_of_process) do
+  def add(%{monitors: monitors} = state, process_pid, kind_of_process) do
     reference = Process.monitor(process_pid)
-
-    Agent.update(monitor_pid, fn state -> Map.put(state, reference, kind_of_process) end)
+    %{state | monitors: Map.put(monitors, reference, kind_of_process)}
   end
 
-  @spec remove(monitor_pid :: pid(), reference()) :: kind_of_process()
+  @spec remove(State.t(), reference()) :: {kind_of_process(), State.t()}
   @doc """
   Stop monitoring given worker or caller process and return kind of it.
   """
-  def remove(monitor_pid, monitoring_reference) do
+  def remove(%{monitors: monitors} = state, monitoring_reference) do
     true = Process.demonitor(monitoring_reference)
-
-    Agent.get_and_update(monitor_pid, fn state ->
-      {Map.get(state, monitoring_reference), Map.delete(state, monitoring_reference)}
-    end)
+    kind_of_process = Map.get(monitors, monitoring_reference)
+    state = %{state | monitors: Map.delete(monitors, monitoring_reference)}
+    {kind_of_process, state}
   end
 end
